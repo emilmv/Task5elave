9fcc51c44fcd0330aa6d93c9bc4194837a0d6af5
emilmv <emilnm@code.edu.az>
2024-02-14 17:13:19 +0400

    Task5Elave

---
+++ Task5Elave/Task5Elave/Program.cs
@@ -0,0 +1,80 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.Design;
+#region Task1
+//int m = 10;
+//int n = 15;
+//int sum = 0;
+//if (m < n)
+//{
+//    for (int i = m; i < n; i++)
+//    {
+//        sum += i;
+//    }
+//    Console.WriteLine(sum);
+//}
+//else
+//{
+//    Console.WriteLine("Duzgun eded daxil edin");
+//}
+#endregion
+#region Task2
+//int n = 1;
+//int m = 100;
+//int count = 0;
+//if (n < m)
+//{
+//    for (int i = n; i < m; i++)
+//    {
+//        if (i % 15 == 0)
+//        {
+//            count++;
+//        }
+//    }
+//    Console.WriteLine(count);
+//}
+//else
+//{
+//    Console.WriteLine("Duzgun eded daxil edin");
+//}
+#endregion
+#region Task3
+//int n = 1;
+//int m = 43;
+//double sum = 0;
+//double count = 0;
+//if (n < m)
+//{
+//    for (int i = n; i < m; i++)
+//    {
+//        if (i % 21 == 0)
+//        {
+//            sum += i;
+//            count++;
+//        }
+//    }
+//    Console.WriteLine(sum/count);
+
+//}
+//else
+//{
+//    Console.WriteLine("Duzgun eded daxl edin");
+//}
+#endregion
+#region Task4
+//int n = 456;
+//int thirdNum = 0;
+//int secondNum = 0;
+//int firstNum = 0;
+
+//if (n > 99 && n < 1000)
+//{
+//    thirdNum += n % 10;
+//    secondNum+= (n / 10) % 10;
+//    firstNum+= (n / 100);
+//    Console.WriteLine(thirdNum + secondNum + firstNum);
+//}
+//else
+//{
+//    Console.WriteLine("Duzgun eded daxil edin");
+//}
+#endregion
